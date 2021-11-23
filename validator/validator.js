e4d8021b02dc26908d18c99a0c991f19a5006336
Shannon Peck <shannon.peck@cornerstonenw.com>
2021-11-23 10:27:10 -0800

    validator now reads JSON schema and JSON data from files

--- validator/validator.js
+++ validator/validator.js
@@ -1,86 +1,43 @@
 'use strict'
-const Ajv = require('ajv');
+const fs = require('fs');
+const path = require('path');
+const Ajv2020 = require('ajv/dist/2020'); //Since I'm using the most current JSON draft, needed to import this Ajv class instead (https://ajv.js.org/json-schema.html#draft-2020-12)
+//const Ajv = require('ajv');
 const addFormats = require("ajv-formats")
+const schema = require('../pet_schema.json'); //JSON schema being read from file
+const input_data = process.argv[2]; //accept data file as an argument, could also 'require' the data as I'm doing with the schema
 
-const ajv = new Ajv();
+const ajv = new Ajv2020();
 addFormats(ajv);
 
-const schema = {
-    "title": "Animal Shelter JSON",
-    "description": "Animal in an animal shelter",
-    "type": "object",
-    "properties": {
-        "petID": {
-            "description": "The unique identifier for the pet",
-            "type": "integer"
-        },
-        "petName": {
-            "description": "Name of pet",
-            "type": "string"
-        },
-        "species": {
-            "description": "Species of animal",
-            "type": "string",
-            "enum": ["Feline", "Canine"]
-        },
-        "breed": {
-            "description": "Breed(s) of pet",
-            "type": "array",
-            "items": {
-                "type": "string"
-            },
-            "minItems": 1,
-            "uniqueItems": true
-        },
-        "markings": {
-            "description": "Coloration and markings of pet",
-            "type": "array",
-            "items": {
-                "type": "string"
-            },
-            "minItems": 1,
-            "uniqueItems": true
-        },
-        "intakeDate": {
-            "description": "Date pet was taken into shelter",
-            "type": "string",
-            "format": "date"
-        },
-        "numIntakes": {
-            "description": "Number of times animal has been in shelter, including current intake",
-            "type": "integer",
-            "minimum": 1
-        }
-    },
-    "required": ["petID", "petName", "species", "breed", "markings", "intakeDate", "numIntakes"]
-};
-
-const goodData = {
-    "petID": 1,
-    "petName": "Emma",
-    "species": "Canine",
-    "breed": ["bulldog"],
-    "markings": ["black", "white chest patch"],
-    "intakeDate": "2011-10-25",
-    "numIntakes": 1
-};
-
-const badData = {
-    "petID": 1,
-    "species": "Canine",
-    "breed": ["bulldog"],
-    "markings": ["black", "white chest patch"],
-    "intakeDate": "2011-10-25",
-    "numIntakes": 1
-};
+function getPath (input) {
+    let reqPath = path.join(__dirname, '..', input);
+    return reqPath;
+}
 
 const validate = ajv.compile(schema);
 
 function isValid(data) {
-    const valid = validate(data);
-    if (!valid) return false;
+    let numInvalid = 0;
+    data.forEach(datum => {
+        const valid = validate(datum);
+        if(!valid)  {
+            //Let user know which object is invalid
+            console.log(`Item number: ${data.indexOf(datum) + 1} invalid`);
+            numInvalid++;
+        }
+    })
+    if (numInvalid > 0) return false;
+    //Return true only if every object matches the schema
     return true;
 }
 
-console.log(isValid(goodData));
-console.log(isValid(badData));
+function readContent () {
+    fs.readFile(getPath(input_data), 'utf-8', (err, data) => {
+        if(err) return console.error("Error!! ", err)
+        let parsed_data = JSON.parse(data);
+        console.log(isValid(parsed_data));
+    })
+}
+
+readContent();
